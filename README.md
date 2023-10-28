281af00422a1497783ed9213d767a508a14dea30
darpbr <dipeixoto@gmail.com>
2023-10-28 16:03:42 -0300

    finalizando curso

--- README.md
+++ README.md
@@ -2,8 +2,21 @@
 
 Projeto criado como parte do curso **PYTHON: entendendo orientação a objetos** da **ALURA**
 
+## Ambiente de desenvolvimento:
+
+- Linux Ubuntu 22.04
+- VSCode
 - Python 3.10.12
 
+## Requisitos
+
+* Toda conta criada deve ser vinculada ao Banco do Brasil:
+* * código do Banco do Brasil = 001
+
+* Uma conta somente pode sacar/transferir um valor se possuir saldo disponível = saldo + limite
+* Toda conta deve iniciar com limite default = 0, a informação de limite pode ser atriuida diretamente na iniciação do objeto ou atribuído posteriormente
+
+## Objeto Conta
 
 - Uma conta deve ter os atributos:
 - - Número de conta
@@ -15,3 +28,4 @@ Projeto criado como parte do curso **PYTHON: entendendo orientação a objetos**
 - - depositar
 - - sacar
 - - extrato
+- - transfere
--- conta.py
+++ conta.py
@@ -1,19 +1,56 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite=0):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        
+    @property
+    def limite(self):
+        return self.__limite
+    
+    @property
+    def numero(self):
+        return self.__numero
 
+    @property
+    def titular(self):
+        return self.__titular
+    
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+    
+    @staticmethod
+    def codigo_banco():
+        return '001'
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001','Caixa':'104','Bradesco':'237'}
+    
     def extrato(self):
-        print('Saldo R$ {} do titular {}'.format(self.saldo, self.titular))
+        print('Saldo R$ {} do titular {}'.format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+    
+    def __pode_sacar_transferir(self, valor):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor <= valor_disponivel
     
     def saca(self, valor):
-        if (self.saldo + self.limite) >= valor:
-            self.saldo -= valor
+        if self.__pode_sacar_transferir(valor):
+            self.__saldo -= valor
         else:
-            print('Saldo insuficiente para sacar! Saldo: R$ {} + limite: R$ {}'.format(self.saldo,self.limite))
+            print('Saldo insuficiente para sacar! Saldo: R$ {} + limite: R$ {}'.format(self.__saldo,self.__limite))
+    
+    def transfere(self, conta_destino, valor):
+        if self.__pode_sacar_transferir:
+            conta_destino.deposita(valor)
+        else:
+            print('Saldo insuficiente para realizar transferência!')
+    
+    def __str__(self):
+        return 'Número: {}, Titular: {}, Saldo: {}'.format(self.__numero, self.__titular, self.__saldo)
